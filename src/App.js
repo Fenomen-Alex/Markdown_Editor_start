5ae78411a364a2bbd35fd3eb64afd0acaf470d54
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-07 21:38:52 +0300

    Completed!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
+// import marked from 'marked';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# sup');
+
+  const handleChange = (e) => setMarkdown(e.target.value);
+
   return (
     <div className="app">
-      <textarea />
+      <textarea
+        value={markdown}
+        onChange={handleChange}
+      />
 
-      <div className="preview" />
+      {/*<div className="preview" dangerouslySetInnerHTML={{__html: marked(markdown)}}/>*/}
+
+      <ReactMarkdown className="preview" children={markdown}/>
+
     </div>
   );
 }
